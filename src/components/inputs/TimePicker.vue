0648c8abd5a66ab26188d0a025ad19ed2d2ccffd
jaanonim <mat8mro@gmail.com>
2021-05-12 13:16:28 +0200

    refactoring dataTable

---
+++ src/components/inputs/TimePicker.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-row>
+    <v-col cols="11" sm="5">
+      <v-menu
+        ref="menu"
+        v-model="menu2"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        :return-value.sync="time"
+        transition="scale-transition"
+        offset-y
+        max-width="290px"
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="time"
+            label="Picker in menu"
+            prepend-icon="mdi-clock-time-four-outline"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-time-picker
+          v-if="menu2"
+          v-model="time"
+          format="24hr"
+          full-width
+          @click:minute="$refs.menu.save(time)"
+        ></v-time-picker>
+      </v-menu>
+    </v-col>
+    <v-col cols="11" sm="5">
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="date"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+            v-model="date"
+            label="Picker in menu"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="date" no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+          <v-btn text color="primary" @click="$refs.menu.save(date)">
+            OK
+          </v-btn>
+        </v-date-picker>
+      </v-menu>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "TimePicker",
+  data() {
+    return {
+      time: null,
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
+      menu2: false,
+    };
+  },
+};
+</script>
